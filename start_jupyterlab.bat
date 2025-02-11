5629f90f8f59128dfa20557f6f53826412d63fb6
hyebinrk/python <gpqls3698@naver.com>
2025-02-11 17:17:57 +0900

    함수, 프로그램입출력

--- start_jupyterlab.bat
+++ start_jupyterlab.bat
@@ -1,4 +1,4 @@
-@echo off
+echo jupyter starting
 echo jupyter starting ....
 timeout 10 /nobreak
 cd d:\HB\code && jupyter lab
--- 예제 숙제/ran.py
+++
@@ -1,5 +0,0 @@
-import random
-menu = ["구내식당",'후문편의점','자가도시락','굶기','과자','엽떡','삼겹살','스시','치킨']
-
-lunch = random.choice(menu)
-print(f' 오늘 선택된 메뉴는 {lunch}를 추천드립니다.')
