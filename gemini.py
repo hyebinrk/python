074d6406d19fe418975c8932605d30ea07b7afa5
hyebinrk/python <gpqls3698@naver.com>
2025-02-26 17:29:29 +0900

    --

--- gemini.py
+++ gemini.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def aiai(text):
     client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
-    response = client.models.generate_content(model="gemini-2.0-flash", contents=text + ";단, 400자 이내 그리고 서술형으로 친절하게 알려줘")   
+    response = client.models.generate_content(model="gemini-2.0-flash", contents=text + ";intj처럼 말해줘")   
     answer = response.text
     print(answer)
     return answer
